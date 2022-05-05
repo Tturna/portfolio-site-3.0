ed0d188d9f17a13193c0dab391e281e7aec76360
Tturna <eemelijuponaho@gmail.com>
2022-05-05 13:44:36 +0300

    projects content and responsive music page

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./Git.css";
 
 class Projects extends Component {
     render() {
@@ -19,9 +20,28 @@ class Projects extends Component {
                     <img id="img" src=".\imgs\avatar_yellow_portrait_small.jpg" alt="avatar icon"/>
                 </div>
 
-                <a href="https://github.com/Tturna" target="_blank" rel="noreferrer">
-                    <img id="projectlink" src=".\imgs\socials\github.png" alt="github logo"/>
-                </a>
+                <div className="gitbox">
+
+                    <a href="https://github.com/Tturna" target="_blank" rel="noreferrer">
+                        <img id="projectlink" src=".\imgs\socials\github.png" alt="github logo"/>
+                    </a>
+
+                    <a className="pin tl" href="https://github.com/Tturna/portfolio-site-3.0" target="_blank" rel="noreferrer">
+                        <img src=".\imgs\gitpins\site.png" alt="github pin" />
+                    </a>
+
+                    <a className="pin tr" href="https://github.com/Tturna/AutoNotifier" target="_blank" rel="noreferrer">
+                        <img src=".\imgs\gitpins\autonotifier.png" alt="github pin" />
+                    </a>
+
+                    <a className="pin bl" href="https://github.com/Tturna/NotSkyrim" target="_blank" rel="noreferrer">
+                        <img src=".\imgs\gitpins\notskyrim.png" alt="github pin" />
+                    </a>
+
+                    <a className="pin br" href="https://github.com/Tturna/ChatBot21" target="_blank" rel="noreferrer">
+                        <img src=".\imgs\gitpins\chatbot.png" alt="github pin" />
+                    </a>
+                </div>
 
             </div>
         );
