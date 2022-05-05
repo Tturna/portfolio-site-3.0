c786db4f9593f2bf17f26cd56e38b46f936bce07
Tturna <42873350+Tturna@users.noreply.github.com>
2022-05-05 22:13:51 +0300

    small detail additions

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {
   BrowserRouter
 } from "react-router-dom";
 
-// import logo from './logo.svg';
 import './App.css';
 import socials from "./socials.json";
 import Home from "./Home";
@@ -67,23 +66,6 @@ function App() {
   };
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
     <BrowserRouter>
       <div className='main'>
 
